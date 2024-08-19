e57ff07f3d7ab3bc3836d836fb27cc08a94c72d3
PNKim <paljapon.nait@gmail.com>
2024-08-19 11:29:30 +0700

    feat: setting tsconfig and cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
